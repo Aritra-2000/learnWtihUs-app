58eab00c64d76bdd25f58f7bfcf4a589c20de9a3
Aritra <112630505+Aritra-2000@users.noreply.github.com>
2023-10-01 12:00:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import {config} from "dotenv";
 import ErrorMiddleware from "./middlewares/Error.js";
 import cookieParser from "cookie-parser";
+import cors from 'cors';
 
 config({
     path: "./config/config.env",
@@ -12,6 +13,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
+app.use(
+   cors({
+      origin: process.env.FRONTEND_URL,
+      credentials: true,
+      methods:["GET","POST","PUT","DELETE"]
+   })
+);
 
 
 import course from "./routes/courseRoutes.js"
@@ -25,5 +33,11 @@ app.use("/api/v1", payment);
 app.use("/api/v1", other);
 
 export default app;
+
+app.get("/", (req, res) =>
+   res.send(
+    `<h1>Click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`
+   )
+);
 
 app.use(ErrorMiddleware)
